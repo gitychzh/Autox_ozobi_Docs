7faabbb2fb995806d46d6d7b28b72aa025c19316
gitychzh <47948154+gitychzh@users.noreply.github.com>
2025-12-01 20:27:08 +0800

    Fix base path format in rspress.config.ts

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "rspress/config";
 export default defineConfig({
     // 文档根目录
     root: "docs",
-    base: "/Autox_ozobi_Docs", // 如果是生成 autox app 的本地文档，需要注释掉这一行
+    base: "/Autox_ozobi_Docs/", // 如果是生成 autox app 的本地文档，需要注释掉这一行
     title: "Autox_ozobi_Docs",
     markdown: {
         showLineNumbers: true,
